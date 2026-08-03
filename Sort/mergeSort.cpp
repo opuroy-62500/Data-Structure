422a2b8c5fdff6a35478330b17b8bc29180f3160
OPU ROY <opuroy2450@gmail.com>
2026-08-03 10:18:40 +0600

    Refactor merge sort implementation with functions

--- Sort/mergeSort.cpp
+++ Sort/mergeSort.cpp
@@ -1,30 +1,67 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-cout<<"Enter the size of the array: ";
-int n;
-cin>>n;
-int arr[n];
-cout<<"Enter the elements of the array: ";
-for(int i = 0; i<n ;i++){
-    cin>>arr[i];
-}
-  // merge sort algorithm
-  for(int i = 1; i<n ;i++){
-    int key = arr[i];
-    int j = i-1;
-    while(j>=0 && arr[j]>key){
-        arr[j+1] = arr[j];
-        j--;
+void Merge(int arr[], int left, int mid, int right)
+{
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    int leftArr[n1];
+    int rightArr[n2];
+
+    for(int i = 0; i < n1; i++)
+        leftArr[i] = arr[left + i];
+
+    for(int j = 0; j < n2; j++)
+        rightArr[j] = arr[mid + 1 + j];
+
+    int i = 0, j = 0, k = left;
+
+    while(i < n1 && j < n2)
+    {
+        if(leftArr[i] <= rightArr[j])
+            arr[k++] = leftArr[i++];
+        else
+            arr[k++] = rightArr[j++];
     }
-    arr[j+1] = key; 
 
+    while(i < n1)
+        arr[k++] = leftArr[i++];
+
+    while(j < n2)
+        arr[k++] = rightArr[j++];
 }
 
-cout<<"Sorted array: "<<endl;
-for(int i = 0; i<n ;i++){   
-    cout<<arr[i]<<" ";  
+void MergeSort(int arr[], int left, int right)
+{
+    if(left >= right)
+        return;
+
+    int mid = left + (right - left) / 2;
+
+    MergeSort(arr, left, mid);
+    MergeSort(arr, mid + 1, right);
+
+    Merge(arr, left, mid, right);
 }
-return 0;
+
+int main()
+{
+    int n;
+    cout << "Enter size: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter elements: ";
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    MergeSort(arr, 0, n - 1);
+
+    cout << "Sorted array: ";
+    for(int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+
+    return 0;
 }
